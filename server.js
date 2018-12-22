017f7baa8715c3cd61dc691beda0be9c0e3c1923
Jack Surtees <jacksurtees@gmail.com>
2018-12-22 16:03:22 -0500

    this is a test

--- server.js
+++ server.js
@@ -272,4 +272,4 @@ if (require.main === module) {
   runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
-module.exports = { runServer, app, closeServer };
+module.exports = { runServer, closeServer, app };
